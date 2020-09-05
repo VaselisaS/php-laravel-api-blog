3eb8553770c210b4c4cdda1eacad2748895b10b1
lisa <lisa.exz@gmail.com>
2020-09-05 17:37:25 +0300

    change directory on Api

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/register', 'AuthController@register');
-Route::post('/login', 'AuthController@login');
-Route::middleware('auth:api')->group(function () {
-    Route::post('/logout', 'AuthController@logout');
+Route::namespace('Api')->group(function () {
+    Route::post('/register', 'AuthController@register');
+    Route::post('/login', 'AuthController@login');
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/logout', 'AuthController@logout');
+    });
 });
