a3cbf5c84c8255f9f663081979683f1c33570ffd
lisa <lisa.exz@gmail.com>
2020-09-10 18:08:45 +0300

    update Laravel v8

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => [], 'as' => 'admin.'], function () {
+    Route::get('/', 'HomeController@index');
+});
+Auth::routes();
 Route::get('/', function () {
     return view('app');
 });
